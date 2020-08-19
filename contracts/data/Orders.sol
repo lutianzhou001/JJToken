653fe864a8c99ee913e927352b8b3603ffc12d75
yljjgroup <tianzhou.lu@jiajiagroup.net>
2020-08-19 21:51:33 +0800

    beautified, but a problem occured

--- contracts/data/Orders.sol
+++ contracts/data/Orders.sol
@@ -11,14 +11,14 @@ contract Orders {
     address public feeAddress;
     address[] public storeAddress;
     address[] public enterpriseAddress;
-
+    
 
     event OrderDelete(string ORDER_ID);
-    event OrderEvent(string ORDER_ID, string USER_ID, string CONSIGNOR, string CONSIGNEE,string CONSIGNEE_TEL,string TRANSACTION_DATE, uint256 TOTAL_PRICE,string  	CONSIGNEE_ADDRESS,string  	STATUS,string  	DELIVERY,string  	CREATE_USER,string  	CREATE_TIME	,string  UPDATE_USER	,string  UPDATE_TIME	,string  ACCOUNT_NAME	,string  ACCOUNT_ID,string  TEL	,string  APP_KEY ,uint256  PAY_MONEY,uint256  SUM_INTEGRAL,string THIRD_ORDER_ID) ;
+    event OrderEvent(string   ORDER_ID,string 	USER_ID	,string  CONSIGNOR,string  	CONSIGNEE,string  	CONSIGNEE_TEL,string  	TRANSACTION_DATE,uint256  	TOTAL_PRICE,string  	CONSIGNEE_ADDRESS,string  	STATUS,string  	DELIVERY,string  	CREATE_USER,string  	CREATE_TIME	,string  UPDATE_USER	,string  UPDATE_TIME	,string  ACCOUNT_NAME	,string  ACCOUNT_ID,string  TEL	,string  APP_KEY ,uint256  PAY_MONEY,uint256  SUM_INTEGRAL,string THIRD_ORDER_ID) ;
 
-
+    
     struct order{
-        string ORDER_ID;
+        string ORDER_ID; 
         string USER_ID;
         string CONSIGNOR;
         string CONSIGNEE;
@@ -40,17 +40,18 @@ contract Orders {
         uint SUM_INTEGRAL;
         string THIRD_ORDER_ID;
     }
-
-    order[] public orders;
-
-    function appendOrder(string memory ORDER_ID, string memory USER_ID, string memory CONSIGNOR, string memory CONSIGNEE, string memory CONSIGNEE_TEL,string memory TRANSACTION_DATE, uint256 TOTAL_PRICE, string memory CONSIGNEE_ADDRESS,string memory STATUS,string memory DELIVERY,string memory CREATE_USER,string memory  CREATE_TIME, string memory UPDATE_USER, string memory UPDATE_TIME, string memory ACCOUNT_NAME, string memory ACCOUNT_ID, string memory TEL, string memory APP_KEY, uint256 PAY_MONEY,uint256  SUM_INTEGRAL,string memory THIRD_ORDER_ID) onlyAdmin external{
+    
+    
+    order[] public orders; 
+   
+    function appendOrder(string memory ORDER_ID, string memory 	USER_ID, string memory CONSIGNOR, string memory CONSIGNEE, string memory CONSIGNEE_TEL, string memory TRANSACTION_DATE, uint256 TOTAL_PRICE, string memory CONSIGNEE_ADDRESS, string memory STATUS, string memory DELIVERY, string memory CREATE_USER, string memory CREATE_TIME	,string memory UPDATE_USER	,string memory UPDATE_TIME	,string memory ACCOUNT_NAME	,string memory ACCOUNT_ID,string memory 	TEL	,string memory APP_KEY	,uint256  PAY_MONEY,uint256 	SUM_INTEGRAL,string memory 	THIRD_ORDER_ID) onlyAdmin external{
         order memory newOrder = order(ORDER_ID	,USER_ID,	CONSIGNOR,	CONSIGNEE,	CONSIGNEE_TEL,	TRANSACTION_DATE,	TOTAL_PRICE	,CONSIGNEE_ADDRESS,	STATUS,	DELIVERY,	CREATE_USER	,CREATE_TIME	,UPDATE_USER	,UPDATE_TIME,	ACCOUNT_NAME,	ACCOUNT_ID,	TEL	,APP_KEY,	PAY_MONEY,	SUM_INTEGRAL	,THIRD_ORDER_ID);
         orders.push(newOrder);
         //emit event
-        emit OrderEvent(ORDER_ID ,USER_ID,	CONSIGNOR,	CONSIGNEE,	CONSIGNEE_TEL,	TRANSACTION_DATE,	TOTAL_PRICE	,CONSIGNEE_ADDRESS,	STATUS,	DELIVERY,	CREATE_USER	,CREATE_TIME	,UPDATE_USER	,UPDATE_TIME,	ACCOUNT_NAME,	ACCOUNT_ID,	TEL	,APP_KEY,	PAY_MONEY,	SUM_INTEGRAL	,THIRD_ORDER_ID);
+        emit OrderEvent(ORDER_ID ,USER_ID, CONSIGNOR, CONSIGNEE, CONSIGNEE_TEL,	TRANSACTION_DATE, TOTAL_PRICE, CONSIGNEE_ADDRESS, STATUS, DELIVERY, CREATE_USER, CREATE_TIME, UPDATE_USER, UPDATE_TIME,	ACCOUNT_NAME, ACCOUNT_ID, TEL, APP_KEY ,PAY_MONEY ,SUM_INTEGRAL, THIRD_ORDER_ID);
     }
-
-
+    
+    
     function getOrderById(string memory ORDER_ID) public view returns (order memory o) {
          for(uint256 i =0; i< orders.length; i++){
            if(compareStrings(orders[i].ORDER_ID , ORDER_ID)){
@@ -58,12 +59,12 @@ contract Orders {
            }
        }
     }
-
-
+    
+    
     function ordersCount() public view returns (uint256 storesCount){
         return orders.length;
     }
-
+    
     function deleteOrder(string memory ORDER_ID) onlyAdmin external returns(bool success){
         for(uint256 i =0; i< orders.length; i++){
            if(compareStrings(orders[i].ORDER_ID , ORDER_ID)){
@@ -75,23 +76,23 @@ contract Orders {
        }
        return false;
    }
-
-
+   
+    
     function compareStrings (string memory a, string memory b) internal pure returns (bool){
        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
     }
-
+   
     function setFeeAddress(address _newFeeAddress) onlyAdmin external{
         require(_newFeeAddress != address(0));
         feeAddress = _newFeeAddress;
     }
-
+    
     function setAdmin(address _newAdmin) external {
         require(_newAdmin != address(0));
         adminAddress = _newAdmin;
     }
-
-
+    
+ 
     modifier onlyAdmin() {
         require(msg.sender == adminAddress);
         _;
